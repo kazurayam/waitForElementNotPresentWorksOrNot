7a8284f61ea07adfe23b73ca21762cea8268984a
kazurayam <kazuaki.urayama@gmail.com>
2023-08-25 16:41:07 +0900

    add Test Cases/subproc; refactor TC1 to use subproc

--- Scripts/TC1/Script1692936068230.groovy
+++ Scripts/TC1/Script1692936068230.groovy
@@ -1,49 +1,11 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.testobject.ConditionType
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
 
-import com.kms.katalon.core.configuration.RunConfiguration
-import java.nio.file.Path
-import java.nio.file.Paths
-import com.kms.katalon.core.util.KeywordUtil
-
-TestObject makeTestObject(String id, String selector) {
-	TestObject tObj = new TestObject(id)
-	tObj.addProperty("css", ConditionType.EQUALS, selector)
-	return tObj
-}
-println "Katalon Studio version: ${RunConfiguration.getAppVersion()}"
-Path projectDir = Paths.get(RunConfiguration.getProjectDir())
-Path html = projectDir.resolve("page.html")
 WebUI.openBrowser('')
 WebUI.setViewPortSize(400, 400)
-WebUI.navigateToUrl(html.toFile().toURI().toURL().toExternalForm() + "?ksVersion=${RunConfiguration.getAppVersion()}")
-TestObject tObj = makeTestObject("demo", "#demo")
-WebUI.verifyElementPresent(tObj, 10, FailureHandling.STOP_ON_FAILURE)
 
-boolean result = WebUI.waitForElementNotPresent(tObj, 10, FailureHandling.STOP_ON_FAILURE)
-println "result=${result}"
+WebUI.callTestCase(findTestCase("subproc"), ["distance": 5, "timeout": 8])
+WebUI.callTestCase(findTestCase("subproc"), ["distance": 30, "timeout": 8])
 
-if (!result) {
-	KeywordUtil.markFailed("element with id=\"demo\" still remains")
-}
-
-WebUI.takeScreenshot("screenshot-" + RunConfiguration.getAppVersion() + ".png")
-WebUI.delay(3);
-//WebUI.closeBrowser()
+WebUI.closeBrowser()
---
+++ Scripts/subproc/Script1692947102896.groovy
@@ -0,0 +1,50 @@
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.util.KeywordUtil
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+assert distance != null
+assert timeout != null
+
+TestObject makeTestObject(String id, String selector) {
+	TestObject tObj = new TestObject(id)
+	tObj.addProperty("css", ConditionType.EQUALS, selector)
+	return tObj
+}
+
+TestObject tObj = makeTestObject("demo", "#demo")
+
+String ksVersion = RunConfiguration.getAppVersion()
+println "Katalon Studio version: ${ksVersion}"
+
+Path projectDir = Paths.get(RunConfiguration.getProjectDir())
+Path outDir = projectDir.resolve("screenshots")
+Files.createDirectories(outDir)
+Path html = projectDir.resolve("page.html")
+StringBuilder sb = new StringBuilder()
+sb.append(html.toFile().toURI().toURL().toExternalForm())
+sb.append("?ksVersion=${ksVersion}")
+sb.append('&')
+sb.append("distance=${distance}")
+String urlString = sb.toString()
+
+WebUI.navigateToUrl(urlString)
+WebUI.verifyElementPresent(tObj, timeout, FailureHandling.STOP_ON_FAILURE)
+				   
+boolean result = WebUI.waitForElementNotPresent(tObj, timeout, FailureHandling.STOP_ON_FAILURE)
+println "result=${result}"
+if (!result) {
+	KeywordUtil.markFailed("element with id=\"demo\" still remains")
+}
+
+Path outfile = outDir.resolve(RunConfiguration.getAppVersion() + "_d" + distance + "_t" + timeout + ".png")
+
+WebUI.takeScreenshot(outfile.toString())
+
+
