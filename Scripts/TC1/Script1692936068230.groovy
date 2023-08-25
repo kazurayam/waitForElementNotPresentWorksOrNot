310bdf441310f2413309f3b538c877fdce97b9c1
kazurayam <kazuaki.urayama@gmail.com>
2023-08-25 13:11:55 +0900

    add Test Cases/TC1

---
+++ Scripts/TC1/Script1692936068230.groovy
@@ -0,0 +1,46 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+import com.kms.katalon.core.configuration.RunConfiguration
+import java.nio.file.Path
+import java.nio.file.Paths
+import com.kms.katalon.core.util.KeywordUtil
+
+TestObject makeTestObject(String id, String selector) {
+	TestObject tObj = new TestObject(id)
+	tObj.addProperty("css", ConditionType.EQUALS, selector)
+	return tObj
+}
+Path projectDir = Paths.get(RunConfiguration.getProjectDir())
+Path html = projectDir.resolve("page.html")
+
+WebUI.openBrowser('')
+WebUI.setViewPortSize(400, 600)
+WebUI.navigateToUrl(html.toFile().toURI().toURL().toExternalForm())
+TestObject tObj = makeTestObject("demo", "#demo")
+
+WebUI.verifyElementPresent(tObj, 10, FailureHandling.STOP_ON_FAILURE)
+
+boolean result = WebUI.waitForElementNotPresent(tObj, 10)
+if (!result) {
+	KeywordUtil.markFailed("element with id=\"demo\" still remains")
+}
+
+WebUI.closeBrowser()
