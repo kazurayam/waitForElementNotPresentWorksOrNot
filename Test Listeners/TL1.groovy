cacfba3417856ad35e51712345031930d1f93c58
kazurayam <kazuaki.urayama@gmail.com>
2023-08-28 17:21:07 +0900

    save progress for wkhtmltopdf

---
+++ Test Listeners/TL1.groovy
@@ -0,0 +1,70 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+
+import internal.GlobalVariable as GlobalVariable
+
+import com.kms.katalon.core.annotation.BeforeTestCase
+import com.kms.katalon.core.annotation.BeforeTestSuite
+import com.kms.katalon.core.annotation.AfterTestCase
+import com.kms.katalon.core.annotation.AfterTestSuite
+import com.kms.katalon.core.context.TestCaseContext
+import com.kms.katalon.core.context.TestSuiteContext
+import com.kms.katalon.core.configuration.RunConfiguration
+import java.nio.file.Path
+import java.nio.file.Paths
+import com.kazurayam.subprocessj.Subprocess
+import com.kazurayam.subprocessj.Subprocess.CompletedProcess
+
+
+class TL1 {
+	
+	@BeforeTestCase
+	def beforeTestCase(TestCaseContext testCaseContext) {
+		//println testCaseContext.getTestCaseId()
+		//println testCaseContext.getTestCaseVariables()
+	}
+
+	@AfterTestCase
+	def afterTestCase(TestCaseContext testCaseContext) {
+		//println testCaseContext.getTestCaseId()
+		//println testCaseContext.getTestCaseStatus()
+	}
+
+	@BeforeTestSuite
+	def beforeTestSuite(TestSuiteContext testSuiteContext) {
+		//println testSuiteContext.getTestSuiteId()
+	}
+
+	@AfterTestSuite
+	def afterTestSuite(TestSuiteContext testSuiteContext) {
+		WebUI.comment("testSuiteId=${testSuiteContext.getTestSuiteId()}")
+		Path reportFolder = Paths.get(RunConfiguration.getReportFolder())
+		WebUI.comment("reportFolder=${reportFolder.toString()}")
+		String prefix = reportFolder.getFileName().toString()
+		String html = prefix + ".html"
+		String pdf  = prefix + ".pdf" 
+		WebUI.comment("html=${html}, pdf=${pdf}")
+		executeWkhtmltopdf(reportFolder, html, pdf)
+	}
+	
+	void executeWkhtmltopdf(Path reportFolder, String html, String pdf) {
+		Subprocess.CompletedProcess cp;
+		cp = new Subprocess().cwd(reportFolder.toFile())
+				.run(Arrays.asList("/usr/local/bin/wkhtmltopdf", html, pdf, "--run-script", "expandAllChildren('s1')"));
+		assert 0 == cp.returncode()
+		cp.stdout().each { line -> println line }
+		cp.stderr().each { line -> println line }
+	}
+}
---
+++ Test Suites/TS2.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>3012e763-44ea-429a-86d3-c63470067a4b</testSuiteGuid>
+   <testCaseLink>
+      <guid>da0319e0-e3b1-40a6-be7e-a1108da0470f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TCx</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
