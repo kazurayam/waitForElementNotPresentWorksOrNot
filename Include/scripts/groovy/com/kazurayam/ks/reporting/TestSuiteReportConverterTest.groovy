c3da660d8bac57c4ebd08453af9962686fd5f79a
kazurayam <kazuaki.urayama@gmail.com>
2023-08-28 22:17:08 +0900

    save progress

--- Include/scripts/groovy/com/kazurayam/ks/reporting/TestSuiteReportConverterTest.groovy
+++ Include/scripts/groovy/com/kazurayam/ks/reporting/TestSuiteReportConverterTest.groovy
@@ -6,19 +6,40 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 @RunWith(JUnit4.class)
 public class TestSuiteReportConverterTest {
+	
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+	private final PrintStream originalOut = System.out;
+	private final PrintStream originalErr = System.err;
+	
+	@Before
+	public void setUpStreams() {
+		System.setOut(new PrintStream(outContent));
+		System.setErr(new PrintStream(errContent));
+	}
+	
+	@After
+	public void restoreStreams() {
+		System.setOut(originalOut);
+		System.setErr(originalErr);
+	}
 
 	@Test
-	void testMultiply() {
+	void test_convertHtmlToPDF() {
 		String ts = "20230828_201457"
 		Path reportFolder = Paths.get("./Include/fixtures/Reports/${ts}/TS2/${ts}")
 		TestSuiteReportConverter instance = new TestSuiteReportConverter()
 		instance.convertHtmlToPDF(reportFolder)
+		println errContent.toString()
+		println outContent.toString()
 		//
 		Path pdfFile = reportFolder.resolve("${ts}.pdf")
 		assertTrue(Files.exists(pdfFile))
--- Keywords/com/kazurayam/ks/reporting/TestSuiteReportConverter.groovy
+++ Keywords/com/kazurayam/ks/reporting/TestSuiteReportConverter.groovy
@@ -1,37 +1,11 @@
 package com.kazurayam.ks.reporting
 
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-
-import com.kms.katalon.core.annotation.Keyword
-import com.kms.katalon.core.checkpoint.Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling
-import com.kms.katalon.core.testcase.TestCase
-import com.kms.katalon.core.testdata.TestData
-import com.kms.katalon.core.testobject.TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import java.nio.file.Path
-import java.nio.file.Paths
 import java.nio.file.Files
+import java.nio.file.Path
 
 import com.kazurayam.subprocessj.Subprocess
-import com.kms.katalon.core.annotation.AfterTestCase
-import com.kms.katalon.core.annotation.AfterTestSuite
-import com.kms.katalon.core.annotation.BeforeTestCase
-import com.kms.katalon.core.annotation.BeforeTestSuite
-import com.kms.katalon.core.configuration.RunConfiguration
-import com.kms.katalon.core.context.TestCaseContext
-import com.kms.katalon.core.context.TestSuiteContext
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.util.KeywordUtil
 
-import internal.GlobalVariable
 
 public class TestSuiteReportConverter {
 
